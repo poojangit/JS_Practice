6b439b52a5a1134e4ba456140e06941891997832
poojangit <poojang147@gmail.com>
2024-11-15 10:07:51 +0530

    Added a day2 practice file

---
+++ Practice/day2.js
@@ -0,0 +1,14 @@
+console.log("Hello Pooja");
+fullName = "Pooja N G"
+age = 24
+address = null
+phone_no = undefined
+married = false
+console.log("Name : "+fullName);
+console.log("Age : "+ age);
+console.log("Address : " + address);
+console.log("phone_no : " + undefined);
+console.log("married : " + married );
+
+
+
