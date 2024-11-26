826013359a5a1ccd585d86fa6cedd1652d624611
poojangit <poojang147@gmail.com>
2024-11-26 17:35:41 +0530

    [POOJA] DataTypes : Practiced on DataTypes of Javascript

--- Practice/day2.js
+++ Practice/day2.js
@@ -10,5 +10,65 @@ console.log("Address : " + address);
 console.log("phone_no : " + undefined);
 console.log("married : " + married );
 
+//Todo ------------DATA TYPES(Primitive)-----------------
+num1 = 55
+num2 = 45.89
+num3 = NaN
+
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(typeof num3);
+
+str1 = "Pooja"
+str2 = 'hi'
+str3 = `I am ${age} old`
+
+console.log(typeof str1);
+console.log(typeof str2);
+console.log(typeof str3);
+
+isAdult = true
+isMarried = false
+
+console.log(typeof isAdult);
+console.log(typeof isMarried);
+
+let pincode = null
+console.log(typeof pincode); // typeof null is Object
+
+let phoneno 
+let parentname = undefined
+console.log(typeof phoneno);
+console.log(typeof parentname );
+
+let uniqueId = Symbol("id")
+let uniqueValue = Symbol("id")
+console.log( typeof uniqueId);
+console.log(typeof uniqueValue);
+console.log(uniqueId);
+console.log(uniqueValue);
+
+console.log(uniqueId == uniqueValue);
+console.log(uniqueId === uniqueValue);
+
+let largeNum = 12221212n
+console.log(typeof largeNum);
+
+// !------------------------------------------------------------------------------
+
+let n1 = "1"
+let n2 = 1
+console.log(n1==n2);
+console.log(n1===n2);
+
+
+
+
+
+
+
+
+
+
 
 
